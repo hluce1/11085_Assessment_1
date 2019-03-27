13c2e619202a7ea5bf55280b0815da6facc2c60b
hluce1 <47615809+hluce1@users.noreply.github.com>
2019-03-27 12:54:04 +1100

    Databases used locally and online

--- 11085app/db-create.sql
+++ 11085app/db-create.sql
@@ -10,3 +10,14 @@ CREATE TABLE crudtable (
     category VARCHAR(30),
 	date TIMESTAMP
 );
+
+CREATE DATABASE epiz_23652834_registration;
+
+use epiz_23652834_registration;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
