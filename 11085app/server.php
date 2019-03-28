5cba824d12d3226b8f5386d24a70e0fd93c3367c
hluce1 <47615809+hluce1@users.noreply.github.com>
2019-03-28 11:39:49 +1100

    Update server.php

--- 11085app/server.php
+++ 11085app/server.php
@@ -1,12 +1,12 @@
 <?php
    session_start();
    
-   // initializing variables
+   // Initializing variables
    $username = "";
    $email    = "";
    $errors = array(); 
    
-   // connect to the database
+   // Connect to the database
    include "config.php";
    $db = mysqli_connect($host, $username, $password, $db_registration);
    //var_dump($db);
@@ -19,8 +19,8 @@
      $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
      $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
    
-     // form validation: ensure that the form is correctly filled ...
-     // by adding (array_push()) corresponding error unto $errors array
+     // Form validation: ensure that the form is correctly filled ...
+     // By adding (array_push()) corresponding error unto $errors array
      if (empty($username)) { array_push($errors, "Username is required"); }
      if (empty($email)) { array_push($errors, "Email is required"); }
      if (empty($password_1)) { array_push($errors, "Password is required"); }
@@ -28,13 +28,13 @@
    	array_push($errors, "The two passwords do not match");
      }
    
-     // first check the database to make sure 
-     // a user does not already exist with the same username and/or email
+     // First check the database to make sure 
+     // A user does not already exist with the same username and/or email
      $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
      $result = mysqli_query($db, $user_check_query);
      $user = mysqli_fetch_assoc($result);
      
-     if ($user) { // if user exists
+     if ($user) { // If user exists
        if ($user['username'] === $username) {
          array_push($errors, "Username already exists");
        }
@@ -46,7 +46,7 @@
    
      // Finally, register user if there are no errors in the form
      if (count($errors) == 0) {
-     	$password = md5($password_1);//encrypt the password before saving in the database
+     	$password = md5($password_1);// Encrypt the password before saving in the database
    
      	$query = "INSERT INTO users (username, email, password) 
      			  VALUES('$username', '$email', '$password')";
@@ -61,7 +61,7 @@
        Check if the user has filled the form correctly.
        Verify that their credentials match the DB.
        Log them in if DB matches.
-       Redirect them to index.php with success message
+       Redirect them to index.php with success message.
    */
    
    // LOGIN USER
